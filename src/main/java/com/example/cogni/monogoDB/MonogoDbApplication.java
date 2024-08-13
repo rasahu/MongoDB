402e2face44704f935ad239ae47af163cb027d98
rasahu <sahu.rakesh.28@gmail.com>
2024-08-14 00:40:33 +0530

    Revert "Updated RestController"
    This reverts commit 6467af8ce196e6e12cfbe0a6b188ab93a53239f3.

--- src/main/java/com/example/cogni/monogoDB/MonogoDbApplication.java
+++ src/main/java/com/example/cogni/monogoDB/MonogoDbApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MonogoDbApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MonogoDbApplication.class, args);
 	}
 
