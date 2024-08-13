47846d7a0210760452484ef138f9a348ed7f03d8
rasahu <sahu.rakesh.28@gmail.com>
2024-08-13 22:27:11 +0530

    first commit

---
+++ src/main/java/com/example/cogni/monogoDB/MonogoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.cogni.monogoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonogoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonogoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cogni/monogoDB/MonogoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cogni.monogoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonogoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
