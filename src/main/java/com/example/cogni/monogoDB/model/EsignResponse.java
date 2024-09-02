ff90cf83ba2423456bcbd8dd94f2185ee1b1dc0e
rasahu <sahu.rakesh.28@gmail.com>
2024-09-02 23:04:32 +0530

    Updated Mongo DB Code

--- src/main/java/com/example/cogni/monogoDB/model/EsignResponse.java
+++ src/main/java/com/example/cogni/monogoDB/model/EsignResponse.java
@@ -2,11 +2,13 @@ package com.example.cogni.monogoDB.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class EsignResponse  extends  GeneralResponse{
    private String packageId;
 }
